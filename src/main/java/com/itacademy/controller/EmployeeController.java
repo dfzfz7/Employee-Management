1c108ff9f1ce46c99ffb521527f081810880d963
Daniel Fernandez <dfzfz7@gmail.com>
2020-09-08 13:22:55 +0200

    HTTP Request errors handling

--- src/main/java/com/itacademy/controller/EmployeeController.java
+++ src/main/java/com/itacademy/controller/EmployeeController.java
@@ -1,15 +1,19 @@
 package com.itacademy.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.itacademy.dto.Employee;
 import com.itacademy.dto.Employee.Job;
@@ -19,16 +23,19 @@ import com.itacademy.service.EmployeeService;
 @Controller
 @RequestMapping("/")
 public class EmployeeController {
-		
+	
+	//List to save employees
+	List<Employee> staff;
+	
 	//Link with Service
 	@Autowired
 	EmployeeService employeeService;
 	
 	//Add employees to list
-	@GetMapping("/index")	
+	@GetMapping("/")	
 	public String showMenu() {
 		//Fill employee list with sample if empty
-		List<Employee> staff = employeeService.getAllEmployees();
+		staff = employeeService.getAllEmployees();
 		if(staff.isEmpty()) { //Add sample employees
 			employeeService.addEmployees();
 		}
@@ -38,11 +45,6 @@ public class EmployeeController {
 	//Get all employees
 	@GetMapping("/staff")
 	public String getAllEmployees(Model model) {
-		//Fill employee list with sample if empty
-		List<Employee> staff = employeeService.getAllEmployees();
-		if(staff.isEmpty()) { //Add sample employees
-			employeeService.addEmployees();
-		}
         staff = employeeService.getAllEmployees();
         model.addAttribute("staff", staff);
         return "showEmployees";
@@ -108,6 +110,23 @@ public class EmployeeController {
 		model.addAttribute("group", group);
 		model.addAttribute("job", job);
 		return "showByJob";
+	}
+	
+	
+	//HANDLING REQUESTS ERRORS
+	
+	// Employee not found handling error - 500
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(NoSuchElementException.class)
+	public String NoEmployeeFound() {
+		return "employeeNotFound";
+	}
+
+	// Invalid number ID handling error - 400
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(NumberFormatException.class)
+	public String invalidId() {
+		return "invalidEmployeeId";
 	}
 	
 }
